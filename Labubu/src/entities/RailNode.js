4910756de45c3270ad64d36c9b08d7c4da39b5ca
tinkiita <146631957+tinkiita@users.noreply.github.com>
2025-11-30 21:11:33 +0100

    Retoques de comentarios

--- Labubu/src/entities/RailNode.js
+++ Labubu/src/entities/RailNode.js
@@ -8,19 +8,13 @@ export class RailNode extends Phaser.Physics.Arcade.Sprite {
         this.y=y;
         
         scene.add.existing(this);
-        scene.physics.add.existing(this, true); // true → body estático
+        scene.physics.add.existing(this, true); 
 
         // Ajustar tamaño 64x64
         this.setDisplaySize(20, 20);
         this.body.setSize(20,20);
         this.setVisible(true);
-        //this.body.setImmovable(true);
 
-        //COLLIDER DEL CENTRO
-        /*this.centerPoint = scene.add.zone(x, y, 20, 20);
-        scene.physics.add.existing(this.centerPoint, true);
-        this.centerPoint.setVisible(true);
-        this.getCenter();*/
     }
     
 }
